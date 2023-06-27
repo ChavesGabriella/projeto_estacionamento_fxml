42961cc15ac086367a7bf1cfe677df92b9a0a8e3
ChavesGabriella <gabriellasoeirooo@gmail.com>
2023-06-27 12:06:08 -0300

    salvar e carregar arquivos.txt

---
+++ src/main/java/ifpr/pgua/eic/exemplo/infra/Escritor.java
@@ -0,0 +1,38 @@
+package ifpr.pgua.eic.exemplo.infra;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import ifpr.pgua.eic.exemplo.models.Veiculo;
+
+public class Escritor {
+    
+    public boolean salvar(String nomeArquivo, ArrayList<Veiculo> lista){
+
+        
+        try {
+            FileWriter fw = new FileWriter(nomeArquivo); //cria oarquivo e sobrescreve
+            BufferedWriter bw = new BufferedWriter(fw); //ecrita no arquivo
+
+            for(Veiculo v: lista){
+                String linha = String.join(";", v.getPlaca(), v.getModelo(), v.getFabricante(), v.getAnoFabricacao()+"", v.getCor());
+                bw.write(linha);
+                bw.newLine();
+            }
+
+            bw.close();
+            fw.close();
+
+            return true;
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+        }
+
+        return false;
+
+    }
+}
